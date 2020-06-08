560dd1ebd8a2c67a46e01ed8855e4103caa01da6
Savva <savva.manylov@bk.ru>
2020-06-08 13:56:06 +0300

    my 4 commit

--- homework.py
+++ homework.py
@@ -1,13 +1,13 @@
 import datetime as dt
 from collections import namedtuple
-Currency = namedtuple('Currency', 'rate name')
+
 class Record:
     
     def __init__(self,amount,comment,date=''):
         self.amount = amount 
         self.comment = comment
         if date == '':
-            self.date = dt.datetime.now().date()
+            self.date = dt.date.today()
         else:
             self.date = dt.datetime.strptime(date, '%d.%m.%Y').date()
 
@@ -21,20 +21,20 @@ class Calculator:
         self.records.append(record)
     
     def get_today_stats(self):
-        today_count = 0
-        for r in self.records:
-            if r.date == dt.datetime.now().date():
-                today_count += r.amount
-        return today_count
+        count = sum(record.amount 
+            for record in self.records 
+                if record.date == dt.date.today()
+            )
+        return count
     
     def get_week_stats(self):
-        week_count = 0
         today=dt.date.today()
         week_ago = today - dt.timedelta(days=7)
-        for r in self.records:
-            if week_ago <= r.date <= today:
-                week_count += r.amount
-        return week_count
+        count_w = sum(record.amount
+            for record in self.records
+                if week_ago <= record.date <= today 
+            )
+        return count_w
     def today_stats(self):
         return self.limit - self.get_today_stats()    
 
@@ -53,10 +53,12 @@ class CashCalculator(Calculator):
             }
         currency_name = CURRENCIES [currency][1]
         currency_rate = CURRENCIES [currency][0]
-        today_stat = super().today_stats()
-        stat = round(today_stat / currency_rate, 2)
-        if stat == 0:
+        today_stat = self.today_stats()
+        if today_stat == 0:
             return ('Денег нет, держись')
+        stat = round(today_stat / currency_rate, 2)
+        if currency == "":
+            raise ("Хозяин,таких денег нет в нашем царстве")
         elif stat > 0:
             return(f'На сегодня осталось {stat} {currency_name}')
         elif stat < 0:
@@ -68,9 +70,12 @@ class CashCalculator(Calculator):
 class CaloriesCalculator (Calculator):
     
     def get_calories_remained(self):
-        stats = super().today_stats()
+        stats = self.today_stats()
         if stats > 0:
-            return(f'Сегодня можно съесть что-нибудь ещё, но с общей калорийностью не более {stats} кКал')
+            return(
+                "Сегодня можно съесть что-нибудь ещё, "
+                   f"но с общей калорийностью не более {stats} кКал"
+            )
         if stats < 0:
             return('Хватит есть!')
 
