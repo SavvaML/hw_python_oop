d0986b17a3944c07de455a71ffe62cb4c44ef9c3
Savva <savva.manylov@bk.ru>
2020-06-11 14:33:08 +0300

    4 commit

--- homework.py
+++ homework.py
@@ -1,13 +1,12 @@
 import datetime as dt
-from collections import namedtuple
-
+DATE = dt.date.today()
 class Record:
     
-    def __init__(self,amount,comment,date=''):
+    def __init__(self,amount,comment,date=None):
         self.amount = amount 
         self.comment = comment
-        if date == '':
-            self.date = dt.date.today()
+        if date == None:
+            self.date = DATE
         else:
             self.date = dt.datetime.strptime(date, '%d.%m.%Y').date()
 
@@ -21,20 +20,19 @@ class Calculator:
         self.records.append(record)
     
     def get_today_stats(self):
-        count = sum(record.amount 
+        return sum(record.amount 
             for record in self.records 
-                if record.date == dt.date.today()
+                if record.date == DATE
             )
-        return count
+        
     
     def get_week_stats(self):
-        today=dt.date.today()
-        week_ago = today - dt.timedelta(days=7)
-        count_w = sum(record.amount
+        week_ago = DATE - dt.timedelta(days=7)
+        return sum(record.amount
             for record in self.records
-                if week_ago <= record.date <= today 
+                if week_ago <= record.date <= DATE
             )
-        return count_w
+        
     def today_stats(self):
         return self.limit - self.get_today_stats()    
 
@@ -45,25 +43,25 @@ class CashCalculator(Calculator):
     RUB_RATE = 1
     
 
-    def get_today_cash_remained(self, currency):
+    def get_today_cash_remained(self, currency=None):
+        today_stat = self.today_stats()
+        if today_stat == 0:
+            return ('Денег нет, держись')
         CURRENCIES = {
             'usd': (self.USD_RATE, 'USD'),
             'eur': (self.EURO_RATE, 'Euro'),
             'rub': (self.RUB_RATE, 'руб')
             }
-        currency_name = CURRENCIES [currency][1]
-        currency_rate = CURRENCIES [currency][0]
-        today_stat = self.today_stats()
-        if today_stat == 0:
-            return ('Денег нет, держись')
-        stat = round(today_stat / currency_rate, 2)
-        if currency == "":
-            raise ("Хозяин,таких денег нет в нашем царстве")
-        elif stat > 0:
-            return(f'На сегодня осталось {stat} {currency_name}')
-        elif stat < 0:
+        if CURRENCIES.get(currency) == None:   
+            raise ValueError ('Хозяин,таких денег нет в нашем царстве')
+        currency=CURRENCIES[currency]        
+        first_currency,second_currency=currency
+        stat = round(today_stat / first_currency, 2)
+        if stat > 0:
+            return(f'На сегодня осталось {stat} {second_currency}')
+        if stat < 0:
             debt = abs(stat)
-            return(f'Денег нет, держись: твой долг - {debt} {currency_name}')
+            return(f'Денег нет, держись: твой долг - {debt} {second_currency}')
 
 
 
@@ -71,11 +69,9 @@ class CaloriesCalculator (Calculator):
     
     def get_calories_remained(self):
         stats = self.today_stats()
-        if stats > 0:
-            return(
-                "Сегодня можно съесть что-нибудь ещё, "
-                   f"но с общей калорийностью не более {stats} кКал"
+        print("Сегодня можно съесть что-нибудь ещё, "
+                f"но с общей калорийностью не более {stats} кКал"
             )
-        if stats < 0:
-            return('Хватит есть!')
-
+        if stats <= 0:
+            print('Хватит есть!')
+            
